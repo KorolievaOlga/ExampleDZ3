ce9ad7a1a333f2079e9f1210236556e8baa00a5c
Olga <olechka.mir28@mail.ru>
2023-05-29 20:44:22 +0300

    Example023

---
+++ Example023/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= a; i++)
+{
+    Console.WriteLine($"Число {i} в кубе {i} = {Math.Pow(i, 3)}");
+}
